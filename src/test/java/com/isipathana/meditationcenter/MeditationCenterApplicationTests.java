5da488be2659f1afa946015a7e4c349c69bfc277
sathirauop <sathira97@gmail.ocm>
2025-09-16 04:21:31 +0530

    Initial commit

---
+++ src/test/java/com/isipathana/meditationcenter/MeditationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isipathana.meditationcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
