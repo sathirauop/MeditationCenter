ca644adfdd4260f96e145629d0309e92212bcd91
sathirauop <sathira97@gmail.ocm>
2025-10-11 19:58:29 +0530

    refactor github actions to add db

--- src/test/java/com/isipathana/meditationcenter/MeditationCenterApplicationTests.java
+++ src/test/java/com/isipathana/meditationcenter/MeditationCenterApplicationTests.java
@@ -2,12 +2,16 @@ package com.isipathana.meditationcenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MeditationCenterApplicationTests {
 
     @Test
     void contextLoads() {
+        // Verifies that the Spring application context loads successfully
+        // with all beans properly configured
     }
 
 }
