4662b9a5816cd1ea341b0196279f9d0355255962
Sathira Shamuditha Basnayake <sathira97@gmail.com>
2025-11-07 16:40:57 +0530

    added the iimc.lk domain into cors mapping list

--- src/main/java/com/isipathana/meditationcenter/config/WebConfig.java
+++ src/main/java/com/isipathana/meditationcenter/config/WebConfig.java
@@ -31,7 +31,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:3001",           // Alternative port
                         "https://isipathana-meditation-center.onrender.com",
                         "https://isipathana-meditation-center-ui.sathira97.workers.dev",
-                        "https://meditationcenterui.onrender.com"// Production backend (for testing)
+                        "https://meditationcenterui.onrender.com",// Production backend (for testing)
+                        "https://iimc.lk"
                         // Add your production frontend URL here when deployed:
                         // "https://your-frontend-domain.com"
                 )
