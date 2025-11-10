1bf2d146f04b17e4f5be944e0812bf05439458f5
sathirauop <sathira97@gmail.ocm>
2025-11-11 01:26:03 +0530

    created the get admin/events end point

---
+++ src/main/java/com/isipathana/meditationcenter/rest/events/admin/get/GetAdminEventsDataAccess.java
@@ -0,0 +1,29 @@
+package com.isipathana.meditationcenter.rest.events.admin.get;
+
+import com.isipathana.meditationcenter.records.event.Event;
+
+import java.util.List;
+
+/**
+ * Data access interface for GetAdminEvents endpoint.
+ * Defines contract for retrieving events with pagination (admin view).
+ *
+ * @author Sathira Basnayake
+ */
+public interface GetAdminEventsDataAccess {
+    /**
+     * Find active events with pagination.
+     *
+     * @param offset the offset to start from
+     * @param limit  the maximum number of events to return
+     * @return list of events
+     */
+    List<Event> findActiveEvents(int offset, int limit);
+
+    /**
+     * Get the total count of active events.
+     *
+     * @return total count of active events
+     */
+    long getActiveEventCount();
+}
---
+++ src/main/java/com/isipathana/meditationcenter/rest/events/admin/get/GetAdminEventsResponseBuilder.java
@@ -0,0 +1,28 @@
+package com.isipathana.meditationcenter.rest.events.admin.get;
+
+import com.isipathana.meditationcenter.models.response.OffsetSearchResponse;
+import com.isipathana.meditationcenter.records.event.Event;
+
+import java.util.stream.Stream;
+
+/**
+ * Response builder interface for GetAdminEvents endpoint.
+ * Transforms Event domain objects to GetAdminEventsResponse DTOs.
+ *
+ * @author Sathira Basnayake
+ */
+public interface GetAdminEventsResponseBuilder {
+    /**
+     * Build paginated response from event stream.
+     *
+     * @param events        stream of events
+     * @param currentOffset current offset in pagination
+     * @param maxOffset     total count of events
+     * @return paginated response
+     */
+    OffsetSearchResponse<GetAdminEventsResponse> build(
+            Stream<Event> events,
+            long currentOffset,
+            long maxOffset
+    );
+}
